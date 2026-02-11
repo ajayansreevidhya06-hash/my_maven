8eab3c88cc2489136e3d75a1df5fd68b79e67769
manoj <manoj@LAPTOP-SC80A1J2.attlocal.net>
2026-02-10 20:10:56 -0600

    New update

---
+++ my_maven/src/main/java/encapsulation/SampleClass.java
@@ -0,0 +1,16 @@
+package encapsulation;
+
+public class SampleClass {
+
+	public static void main(String[] args) 
+	
+	{
+		EncapsulationSampleClass ob =new EncapsulationSampleClass();
+		ob.setter("Sreevidhya",1111);
+		ob.getter();
+		System.out.println("Git Update");
+		
+
+	}
+
+}
